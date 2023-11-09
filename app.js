a284ff661e8225cb3e99ec5397526feebcfeed29
GeEuzebio <georgeeuzebio@gmail.com>
2023-11-09 13:47:33 -0300

    maintence

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const belvo = require("belvo").default;
 const path = require("path");
-const fetch = require("node-fetch")
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const mysql = require("mysql2");
 
 const app = express();
 const PORT = 3000;
 
+var client = new belvo(
+    '523aaabe-3dce-49ea-bb14-b735b6ee260a',
+    't-hdVW@gcWjDF@b7QuCqBm1FYOCqJbQ2dqGKBYEQblEso2dIWwI_UhKehbs2J@EK',
+    'sandbox'
+);
+
+var userEmail = '';
+
+function setUserEmail(email){
+    userEmail = email;
+}
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '210692',
+    database: 'bravcon'
+})
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
@@ -12,62 +37,92 @@ app.get('/', (req, res) => {
 });
 
 app.get('/get-access-token', (req, res) => {
-    fetch('https://sandbox.belvo.com/api/token/', {
+    fetch('https://development.belvo.com/api/token/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Host': 'sandbox.belvo.com',
+            'Host': 'development.belvo.com',
         },
         body: JSON.stringify({
-            id: "523aaabe-3dce-49ea-bb14-b735b6ee260a",
-            password: "t-hdVW@gcWjDF@b7QuCqBm1FYOCqJbQ2dqGKBYEQblEso2dIWwI_UhKehbs2J@EK",
-            scopes: "read_institutions,write_links,read_consents,write_consents,write_consent_callback,delete_consents",
-            widget: {
-                branding: {
-                    company_icon: "https://raw.githubusercontent.com/GeEuzebio/Bravcon/aec5516f9f2285cfcf6a954ee97db1633032eeca/img/bravcon_icon.svg",
-                    company_logo: "https://raw.githubusercontent.com/GeEuzebio/Bravcon/aec5516f9f2285cfcf6a954ee97db1633032eeca/img/bravcon_icon.svg",
-                    company_name: "Bravcon",
-                    social_proof: true,
-                    company_terms_url: "https://belvo.com/terms-service/",
-                    company_benefit_header: "Faster approvals",
-                    company_benefit_content: "Using Belvo cuts down on your loan approval time by up to 15 days.",
-                    opportunity_loss: "It can take up to 20 days to evaluate your request using traditional methods.",
-                    overlay_background_color: "#292928"
-                },
-                openfinance_feature: "consent_link_creation",
-                callback_urls: {
-                    success: "https://www.belvo.com"
-                },
-                consent: {
-                    terms_and_conditions_url: "https://www.belvo.com",
-                    permissions: [
-                        "REGISTER",
-                        "ACCOUNTS",
-                        "CREDIT_CARDS",
-                        "CREDIT_OPERATIONS"
-                    ],
-                    identification_info: [
-                        {
-                            type: "CPF",
-                            number: "03260162364",
-                            name: "George Euzébio"
-                        }
-                    ]
-                }
-            }
+            "id": "28094c13-1a0c-43a8-97c1-a59df45b9251",
+            "password": "-nTod8RtkgZ@j@n2ijrvP4eX@jw@4Mwf0Els3uUnam0arrdJ21#USkH2O#f8fCdz",
+            "scopes": "read_institutions,write_links"
         })
     })
-        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
         .then(data => {
             const refreshToken = data.refresh;
             const accessToken = data.access;
+            console.log(accessToken);
             res.json({ access: accessToken });
         })
         .catch(error => {
             console.error('Error:', error);
-            res.status(500).json({ error: 'Erro ao obter o token de acesso' });
         });
 });
+
+app.post('/send-link', (req, res) => {
+    const result = req.body;
+    console.log(result);
+    fetch(`https://sandbox.belvo.com/api/accounts/${result.link}/?fields=id`, {method: 'GET'})
+    .then(response => {
+        console.log(response.status);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.log('Erro: ' + error));
+    console.log(userEmail);
+    connection.connect();
+    connection.query('UPDATE users SET linkId=? WHERE email=?', [result.link, userEmail]);
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'register_screen.html'));
+});
+
+app.post('/register', (req, res) => {
+    const form = req.body;
+    if (form.password === form.confirmPassword) {
+        connection.connect((err) => {
+            if (err) {
+                console.log('Erro ao se conectar: ' + err.stack);
+                return;
+            }
+            console.log('Conexão bem-sucedida!');
+        })
+
+        const newUser = {
+            nome: form.firstName,
+            sobrenome: form.lastName,
+            email: form.email,
+            senha: form.password,
+            linkId: ''
+        }
+
+        connection.query('INSERT INTO users SET ?', newUser);
+    } else {
+        console.log('Senhas diferentes')
+    }
+    res.redirect('/login');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const user = req.body;
+    connection.connect();
+    connection.query('SELECT * FROM users WHERE email=? AND senha=?', [user.email, user.password], (result) => {
+        setUserEmail(user.email);
+        res.redirect('/');
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
